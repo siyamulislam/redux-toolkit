8b5a60f55953337e340f2f2bf54bdfac43ae8be5
siyamulislam <siyamulislam1@gmail.com>
2023-08-22 00:23:15 +0600

    applymiddleware logger

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-import { createStore } from 'redux';
-const store = createStore(reducer);
-const history=[]
+import { createStore , applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+// Constants for action types
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const INCREMENT_BY_AMOUNT = 'INCREMENT_BY_AMOUNT';
+const store = createStore(counterReducer,applyMiddleware(logger.default));
+const history=[] 
+
 //reducer
-function reducer(state={bal:0}, action) {
-    if(action.type==='increment'){
-        return {bal:state.bal+1};
-    // state.bal=state.bal+1;
-    }
-    return  state ;
+function counterReducer(state={balance:0}, action) {
+    switch (action.type) {
+        case INCREMENT:
+          return { balance: state.balance + 1 };
+        case DECREMENT:
+          return { balance: state.balance - 1 };
+        case INCREMENT_BY_AMOUNT:
+          return { balance: state.balance + action.payload };
+        default:
+          return state;
+      }
 }
 //global this.state
 
-store.subscribe(()=>{
-    history.push(store.getState())
-    console.log(history)
-    // console.log(store.getState())
-})
+// store.subscribe(()=>{
+//     history.push(store.getState())
+//     console.log(history)
+//     // console.log(store.getState())
+// })
+// Action creators
+function increment() {
+    return { type: INCREMENT };
+  }
+  
+  function decrement() {
+    return { type: DECREMENT };
+  }
+  
+  function incrementByAmount(value) {
+    return { type: INCREMENT_BY_AMOUNT, payload: value };
+  }
+
+
+// Set up an interval to dispatch actions
 setInterval(() => {
-store.dispatch({type:'increment'})
-}, 1000);
+// store.dispatch({type: increment})
+// store.dispatch({type: decrement})
+// store.dispatch({type: incrementByAmount, payload:90})
+store.dispatch(incrementByAmount(50))
+}, 3000);
 
  
